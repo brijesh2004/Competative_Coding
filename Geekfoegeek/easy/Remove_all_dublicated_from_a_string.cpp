e0521b31ebe2dd5452ec1139db17f76db53ba495
brijeshsingh <brijesh122004@gmail.com>
2022-08-25 11:16:47 +0530

    adding a file

---
+++ Geekfoegeek/easy/Remove_all_dublicated_from_a_string.cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+	string removeDuplicates(string str) {
+	    // code here
+	    string s;
+	    int length = str.length();
+	    for(int i=0;i<length;i++){
+	        char c= str[i];
+	        auto found = s.find(c);
+	        if(found==std::string::npos){
+	            s+=c;
+	        }
+	    }
+	    return s;
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string str;
+        cin >> str;
+        Solution ob;
+        auto ans = ob.removeDuplicates(str);
+
+        cout << ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
